594a30c62f1095b1eb39a1d13ecf81296c93e593
Antonio de Jesus Perez Vicencio <71039689+Antoniopv7@users.noreply.github.com>
2024-11-10 19:22:24 -0300

    Migraciones para la BD, modificaciones en la tabla convenio

---
+++ Migrations/20241110220100_UpdateConvenioModel.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gestion_Del_Presupuesto.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateConvenioModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CentrosDeSaludId",
+                table: "Convenios");
+
+            migrationBuilder.DropColumn(
+                name: "Id_Retribucion",
+                table: "Convenios");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CentrosDeSaludId",
+                table: "Convenios",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id_Retribucion",
+                table: "Convenios",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
