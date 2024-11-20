f42424216b4e4eb66e9f60bf744fe737b15a8113
Antonio de Jesus Perez Vicencio <71039689+Antoniopv7@users.noreply.github.com>
2024-11-20 20:41:10 -0300

    agregado modelo register y modificacion del modelo usuario

---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Gestion_Del_Presupuesto.Models;
+
+namespace Gestion_Del_Presupuesto.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [Display(Name = "Nombre de Usuario")]
+        public string NombreUsuario { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "La contraseña debe tener al menos 6 caracteres.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Contraseña")]
+        public string Contraseña { get; set; }
+
+        [Required]
+        [Compare("Contraseña", ErrorMessage = "Las contraseñas no coinciden.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirmar Contraseña")]
+        public string ConfirmarContraseña { get; set; }
+
+        [Required]
+        [Display(Name = "Rol")]
+        public int Id_Rol { get; set; }
+
+        // Propiedades adicionales para Rut y Teléfono
+        [Required]
+        [Display(Name = "RUT")]
+        public string Rut { get; set; }
+
+        [Required]
+        [Phone]
+        [Display(Name = "Número de Teléfono")]
+        public string Telefono { get; set; }
+
+        // Lista de roles para mostrar en el formulario
+        public List<Rol> Roles { get; set; }
+    }
+}
--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,22 +1,41 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Gestion_Del_Presupuesto.Models
 {
     public class Usuario
     {
         [Key]
-        public int Id_Usuario { get; set; }  
+        public int Id_Usuario { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre de Usuario")]
         public string Nombre { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Contraseña { get; set; }
 
+        [Required]
+        public int Id_Rol { get; set; }
 
-        public int Id_Rol { get; set; }  
-        public virtual Rol Rol { get; set; }  
+        // Nuevas propiedades
+        [Required]
+        [Display(Name = "RUT")]
+        public string Rut { get; set; }
 
+        [Required]
+        [Phone]
+        [Display(Name = "Número de Teléfono")]
+        public string Telefono { get; set; }
+
+        // Relación con el rol
+        public Rol Rol { get; set; }
 
         public virtual ICollection<Historial_Actividad> Historial_Actividades { get; set; }  // Relación con Historial_Actividad
-
-        
     }
 }
